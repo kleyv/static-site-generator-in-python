7d880836e0e2e3a4d15527d8008b3320947abca9
user <user@mail.com>
2025-11-28 11:47:47 +0000

    Remove unnecessary text

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -24,10 +24,3 @@ class TestTextNode(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
-
-# This test creates two TextNode objects with the same properties and asserts that they are equal. Notice the missing url argument which should have a default value of None. If you run your tests with ./test.sh, you should see that the test passes.
-
-# Add some test cases by adding methods to the TestTextNode class to verify that the TextNode class works as expected. You can use the following methods to compare the objects:
-# self.assertEqual - if the inputs are equal the test passes
-# self.assertNotEqual - if the inputs are not equal the test passes
-# Add even more test cases (at least 3 in total) to check various edge cases, like when the url property is None, or when the text_type property is different. You'll want to make sure that when properties are different, the TextNode objects are not equal.
