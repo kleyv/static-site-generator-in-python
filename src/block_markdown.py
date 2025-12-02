413965a1c619ab78c0930cb2fb58800b347e4bb7
user <user@mail.com>
2025-12-02 09:18:00 +0000

    Add BlockType, block_to_block_type and tests

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,4 +1,5 @@
 from enum import Enum
+import re
 
 def markdown_to_blocks(markdown):
     blocks = map(lambda block: block.strip(),markdown.split("\n\n"))
@@ -6,6 +7,70 @@ def markdown_to_blocks(markdown):
     return list(filter_empty_strings)
 
 
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
+def block_to_block_type(markdown_text_block):
+    lines = list(filter(lambda line: len(line) > 0,markdown_text_block.split("\n")))
+    
+    # starts with 1-6 #
+    is_heading = all(
+        map(lambda line: re.match(r"(^#{1,6}\s)", line), lines)
+    )
+    if is_heading:
+        return BlockType.HEADING
+
+    # starts and ends with ````
+    first_and_last_line = [lines[0],lines[-1]]
+    is_code_block = all(
+        map(lambda line: re.match(r"(^```)", line) ,first_and_last_line)
+    )
+    if is_code_block:
+        return BlockType.CODE
+
+    # starts with >
+    is_quote = all(list(
+        map(lambda line: re.match(r"(^>)", line), lines)
+    ))
+    if is_quote:
+        return BlockType.QUOTE
+    
+    # line starts with hypen-space
+    is_unordered_list = all(map(lambda line: re.match(r"(^-\s)", line),lines))
+    if is_unordered_list:
+        return BlockType.UNORDERED_LIST
+    
+    ordered_list_rules = []
+    for i in range(len(lines)):
+        # start witha  digit-dot-space
+        line_starts_with_digit_dot_space = re.match(r"(^\d\.\s)", lines[i])
+        if i == 0:
+            ordered_list_rules.append(re.match(r"(^\d\.\s)", lines[i]))
+        elif line_starts_with_digit_dot_space:
+            previous_digit = int(re.match(r"(^\d)", lines[i-1])[0])
+            current_digit = int(re.match(r"(^\d)", lines[i])[0])
+            condition = line_starts_with_digit_dot_space and (current_digit == previous_digit + 1)
+            ordered_list_rules.append(condition)
+        else:
+            ordered_list_rules.append(False)
+    is_ordered_list = all(ordered_list_rules)
+    if is_ordered_list:
+        return BlockType.ORDERED_LIST
+
+    return BlockType.PARAGRAPH
+    
+# md = """
+# ```
+# let message = 'Hello world';
+# alert(message);
+# ```
+# """
+# print(block_to_block_type(md))
 # md = """
 # This is **bolded** paragraph
 
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,5 +1,9 @@
 import unittest
-from block_markdown import markdown_to_blocks
+from block_markdown import (
+    markdown_to_blocks,
+    block_to_block_type,
+    BlockType
+)
 
 class TestMarkdownToBlocks(unittest.TestCase):
     def test_markdown_to_blocks(self):
@@ -48,6 +52,27 @@ You may be using [Markdown Live Preview](https://markdownlivepreview.com/).
                 "1. Item 1\n2. Item 2\n3. Item 3\n    1. Item 3a\n    2. Item 3b"
             ],
         )
+
+    def test_block_to_block_type_code(self):
+        block_of_text = """
+```
+let message = 'Hello world';
+alert(message);
+```
+"""
+        block_type = block_to_block_type(block_of_text)
+        self.assertEqual(block_type, BlockType.CODE)
+
+    def test_block_to_block_type_paragraph(self):
+        block_of_text = """
+mo
+> Markdown is a lightweight markup language with plain-text-formatting syntax, created in 2004 by John Gruber with Aaron Swartz.  
+## Second section
+>> Markdown is often used to format readme files, for writing messages in online discussion forums, and to create rich text using a plain text editor.message);
+```
+"""
+        block_type = block_to_block_type(block_of_text)
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
 
 if __name__ == "__main__":
     unittest.main()
